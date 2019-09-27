d4b0074b430107b39e8ac5f35d3650a9e5a2e4e2
RalphUnity <55875029+RalphUnity@users.noreply.github.com>
2019-09-28 04:35:19 +0800

    Text Damage and Animation
    Add text damage and animation on enemies

---
+++ Project KoK/Assets/Scripts/Cyber/FloatingText.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingText : MonoBehaviour
+{
+
+    public float destroyTime = 3f;
+    public Vector3 offSet = new Vector3(0, 2, 0);
+    public Vector3 randomizeIntensity = new Vector3(0.5f, 0, 0);
+    public 
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, destroyTime);
+
+        transform.localPosition += offSet;
+        transform.localPosition += new Vector3(Random.Range(-randomizeIntensity.x, randomizeIntensity.x),
+        Random.Range(-randomizeIntensity.y, randomizeIntensity.y),
+        Random.Range(-randomizeIntensity.z, randomizeIntensity.z));
+    }
+
+    void Update()
+    {
+        TextMesh floatTextMesh = GetComponent<TextMesh>();
+        floatTextMesh.transform.rotation = Camera.main.transform.rotation;
+    }
+}
--- Project KoK/Assets/Scripts/Cyber/Target.cs
+++ Project KoK/Assets/Scripts/Cyber/Target.cs
@@ -4,6 +4,7 @@ public class Target : MonoBehaviour
 {
 
     public float health = 50f;
+    public GameObject floatingTextPrefab;
 
     public void TakeDamage(float amount)
     {
@@ -12,10 +13,18 @@ public class Target : MonoBehaviour
         {
             Die();
         }
+
+        if (floatingTextPrefab && health > 0)
+        {
+            var floatTextGo = Instantiate(floatingTextPrefab, transform.position, Quaternion.identity, transform);
+            floatTextGo.GetComponent<TextMesh>().text = amount.ToString();
+        }
+        
     }
 
     void Die()
     {
         Destroy(gameObject);
     }
+
 }
