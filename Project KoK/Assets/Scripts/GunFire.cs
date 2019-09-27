ba7abf853b73ac71bd7e54a293f5693f6332bae1
RalphUnity <55875029+RalphUnity@users.noreply.github.com>
2019-09-27 19:58:01 +0800

    Second project commit
    Uploaded the second exisiting project

---
+++ Project KoK/Assets/Scripts/GunFire.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunFire : MonoBehaviour
+{
+
+    public GameObject Flash;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GlobalAmmo.LoadedAmmo >= 1)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                AudioSource gunSound = GetComponent<AudioSource>();
+                gunSound.Play();
+                Flash.SetActive(true);
+                StartCoroutine(MuzzleOff());
+                GetComponent<Animation>().Play("GunShot");
+                GlobalAmmo.LoadedAmmo -= 1;
+            }
+        }
+    }
+
+    public IEnumerator MuzzleOff()
+    {
+        yield return new WaitForSeconds(0.15f);
+        Flash.SetActive(false);
+    }
+}
